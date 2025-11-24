08a07a39c582e1a2a2ed7a26fc761a5067b2fc15
Stevel9-le <stevenle24ti@mahasiswa.pcr.ac.id>
2025-11-24 13:44:28 +0700

    2

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -88,8 +88,8 @@
                                 @endforeach
                             </tbody>
                         </table>
-                        {{-- <div class="mt-3">
-                            {{ $dataPelanggan->links('pagination::bootstrap-5') }} --}}
+                        <div class="mt-3">
+                            {{ $dataPelanggan->links('pagination::bootstrap-5') }}
                         </div>
                     </div>
                 </div>
