d599eda77662c00b4938c9e6cb70a56fd0e43401
Stevel9-le <stevenle24ti@mahasiswa.pcr.ac.id>
2025-11-24 14:03:19 +0700

    membuat nomor berurutan

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -51,7 +51,7 @@
                             <tbody>
                                 @foreach ($dataPelanggan as $item)
                                     <tr>
-                                        <td>{{($dataPelanggan->currentPage() - 1) * $dataPelanggan->perPage() + $item->iteration}}</td>
+                                        <td>{{($dataPelanggan->currentPage() - 1) * $dataPelanggan->perPage() + $loop->iteration}}</td>
                                         <td>{{ $item->first_name }}</td>
                                         <td>{{ $item->last_name }}</td>
                                         <td>{{ $item->birthday }}</td>
